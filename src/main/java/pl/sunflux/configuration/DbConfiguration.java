94a4d9d93cbf22eb1de9be297109a00a1c1811c2
LysanderPL <m.kruszewski@sunflux.pl>
2017-09-14 17:35:55 +0200

    configuration

---
+++ src/main/java/pl/sunflux/configuration/DbConfiguration.java
@@ -0,0 +1,12 @@
+package pl.sunflux.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Created by maciej on 13.09.17.
+ */
+@Configuration
+@EnableJpaRepositories
+public class DbConfiguration {
+}
---
+++ src/main/java/pl/sunflux/configuration/WebConfiguration.java
@@ -0,0 +1,12 @@
+package pl.sunflux.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by maciej on 13.09.17.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfiguration {
+}
